aa791724db64e0577bcf6167fe0f1a745a455b1e
jordane101 <elijordan282@gmail.com>
2025-07-28 03:24:23 +0000

    hidden markov models

--- test/alpaca_historical_test.py
+++ test/alpaca_historical_test.py
@@ -3,14 +3,14 @@ from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
 import pandas as pd
 
-client = StockHistoricalDataClient("PKP6NX8RZUI9HJR5VLZ3","OHgpNCENWdYXEdD8fyBbyDW7hqXUTkDmyfhAiZ14")
+client = StockHistoricalDataClient("PKJHI0WM4W283UN7RD0O","Zb1AoNcu2Bc5JE62PqarFzr8s7Fhkc8BOGM8E3dW")
 
 symbol = "NVDA"
 
 request_params = StockBarsRequest(
     symbol_or_symbols=[symbol],
     timeframe=TimeFrame.Day,
-    start="2020-01-01"
+    start="2023-01-01"
 )
 
 bars = client.get_stock_bars(request_params)
